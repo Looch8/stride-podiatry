3a816d525371baadd33e69452b1fc4d043a01b89
Luke Wheldale <lukewheldale@gmail.com>
2024-12-10 14:26:54 +1030

    bg color

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -45,7 +45,7 @@ export default function Homepage() {
         <body class="h-full">
         ```
       */}
-			<div className="min-h-full">
+			<div className="min-h-full bg-blue-300">
 				<Disclosure as="nav" className="bg-gray-800">
 					<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 						<div className="flex h-16 items-center justify-between">
