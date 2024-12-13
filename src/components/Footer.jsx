453eb33f9ba90c2eaebc563765430ddab547a082
Luke Wheldale <lukewheldale@gmail.com>
2024-12-13 10:51:07 +1030

    added facebook/instagram icon links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,24 @@
+import '../styles/ContactStyles.css';
+
 const Footer = () => {
 	return (
 		<footer className="footer">
+			<div className="social-links">
+				<a
+					href="https://www.facebook.com"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<i className="fab fa-facebook-square"></i>
+				</a>
+				<a
+					href="https://www.instagram.com"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<i className="fab fa-instagram"></i>
+				</a>
+			</div>
 			<p>&copy; 2024 Stride Podiatry. All Rights Reserved.</p>
 		</footer>
 	);
