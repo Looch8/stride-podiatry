9f4bb008a2dbecc6b061afbd89a7647de287d6bb
Luke Wheldale <lukewheldale@gmail.com>
2025-01-29 20:33:39 +1030

    change homepage and font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,20 +5,31 @@ const Home = () => {
 	return (
 		<main className="main-content">
 			<section className="home-section">
-				<h2>Welcome to Stride Podiatry</h2>
-				<h3>Servicing all of Adelaide, South Australia.</h3>
+				<h2>Stride Podiatry – We Come to You!</h2>
+				<h3>Professional Mobile Podiatry Across Adelaide</h3>
 				<p>
-					We offer home visits for a range of podiatry services,
-					including assessments, treatments, and care for conditions
-					such as bunions, ingrown toenails, and plantar fasciitis.
+					No referral is necessary to see a podiatrist—
+					<strong>book directly with us today!</strong>
 				</p>
+				<p>
+					We are a <strong>mobile podiatry service</strong>, meaning
+					we treat you in the comfort of your home, workplace, or aged
+					care facility . Our goal is to provide high-quality,
+					convenient care wherever you need it.
+				</p>
+				<p>
+					Stride Podiatry serves{' '}
+					<strong>all of the Adelaide metro area</strong>. If you’re
+					outside this region, feel free to contact us—{' '}
+					<em>we may still be able to help.</em>
+				</p>
+
 				<div className="book-now">
 					<h2>Ready to Take the First Step?</h2>
 					<p>
-						Book your home visit appointment today and enjoy
-						professional podiatry care in the comfort of your home!
+						Book your <strong>home visit appointment</strong> today
+						and experience expert podiatry care at your convenience.
 					</p>
-
 					<Link to="/booking" className="book-now-button">
 						Book Now
 					</Link>
