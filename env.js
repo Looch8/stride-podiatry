b3c36737616a1e4b1f49f57438f93ee893641222
Luke Wheldale <lukewheldale@gmail.com>
2025-05-20 16:07:30 +0930

    Updates

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 window.env = {
-	VITE_EMAILJS_SERVICE_ID: 'service_w3e776c',
+	VITE_EMAILJS_SERVICE_ID: 'service_6le6z7l',
 	VITE_EMAILJS_TEMPLATE_ID: 'template_72brrc8',
 	VITE_EMAILJS_PUBLIC_KEY: 'eo5Pn_6dmTlVyFtN_',
 	VITE_EMAILJS_BOOKING_TEMPLATE_ID: 'template_fa98dlh',
